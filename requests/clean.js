086b6439e907075068cf5c793180144fbae4b8b0
Joshua <joshuacfang@gmail.com>
2017-04-18 06:36:09 -0400

    change to unlink sync

--- requests/clean.js
+++ requests/clean.js
@@ -6,6 +6,6 @@ const valid = Object.keys(index).map(key => `${index[key]}.txt`);
 
 fs.readdirSync(path.join(__dirname, 'raw')).forEach((file) => {
 	if (valid.indexOf(file) < 0) {
-		fs.unlink(path.join('raw', file));
+		fs.unlinkSync(path.join('raw', file));
 	}
 });
