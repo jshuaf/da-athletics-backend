5fcd5bc9439718a69ea4e823ce271946cf399662
Joshua <joshuacfang@gmail.com>
2017-04-23 22:51:24 -0400

    fix logging

--- model/patch.js
+++ model/patch.js
@@ -4,14 +4,17 @@ const co = require('co');
 model.connect().then(model.findAllTeams)
 .then((teams) => {
 	teams.forEach((team) => {
-		console.log(team._id);
-		const newEvents = [];
-		team.events.forEach((eventID) => {
-			co(function* () {
-				if (newEvents.indexOf(eventID) < 0) newEvents.append(eventID);
+		co(function* () {
+			const program = yield model.findProgram({ _id: team.program, });
+			console.log(program.name, team.level);
+			const newEvents = [];
+			team.events.forEach((eventID) => {
+				co(function* () {
+					if (newEvents.indexOf(eventID) < 0) newEvents.append(eventID);
+				});
 			});
+			team.events = newEvents;
+			model.updateTeam(team._id, team);
 		});
-		team.events = newEvents;
-		model.updateTeam(team._id, team);
 	});
 });
