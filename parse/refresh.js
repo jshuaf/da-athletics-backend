1c203a6bbd7bfb5fcc70eccbeb486a195d74846f
Joshua <joshuacfang@gmail.com>
2017-05-09 20:32:17 -0400

    fix refreshing

--- parse/refresh.js
+++ parse/refresh.js
@@ -2,42 +2,22 @@ const cheerio = require('cheerio');
 const parse = require('./parse.js');
 const requests = require('../requests/requests');
 
-module.exports.recent = () => {
-	requests.get('https://deerfield.edu/athletics/events/recent-scores/', { update: true, })
-	.then(({ data, }) => {
-		parse.refreshEvents(cheerio.load(data))
-		.then(() => {
-			console.log('Refreshed all recent scores.');
-		});
+module.exports.recent = () => requests.get('https://deerfield.edu/athletics/events/recent-scores/', { update: true, })
+	.then(({ data, }) => parse.refreshEvents(cheerio.load(data))).then(() => {
+		console.log('Refreshed all recent scores.');
 	});
-};
 
-module.exports.test = () => {
-	requests.get('https://deerfield.edu/athletics/teams/junior-varsity-boys-tennis/', { update: true, })
-	.then(({ data, }) => {
-		parse.refreshEvents(cheerio.load(data))
-		.then(() => {
-			console.log('Refreshed test team.');
-		});
+module.exports.test = () => requests.get('https://deerfield.edu/athletics/teams/junior-varsity-boys-tennis/', { update: true, })
+	.then(({ data, }) => parse.refreshEvents(cheerio.load(data))).then(() => {
+		console.log('Refreshed test team.');
 	});
-};
 
-module.exports.upcoming = () => {
-	requests.get('https://deerfield.edu/athletics/events/', { update: true, })
-	.then(({ data, }) => {
-		parse.refreshEvents(cheerio.load(data))
-		.then(() => {
-			console.log('Refreshed all upcoming scores.');
-		});
+module.exports.upcoming = () => requests.get('https://deerfield.edu/athletics/events/', { update: true, })
+	.then(({ data, }) => parse.refreshEvents(cheerio.load(data))).then(() => {
+		console.log('Refreshed all upcoming scores.');
 	});
-};
 
-module.exports.all = (year) => {
-	requests.get(`https://deerfield.edu/athletics/events/${year}`, { update: true, })
-		.then(({ data, }) => {
-			parse.refreshEvents(cheerio.load(data))
-			.then(() => {
-				console.log(`Refreshed scores for ${year}.`);
-			});
+module.exports.all = year => requests.get(`https://deerfield.edu/athletics/events/${year}`, { update: true, })
+		.then(({ data, }) => parse.refreshEvents(cheerio.load(data))).then(() => {
+			console.log(`Refreshed scores for ${year}.`);
 		});
-};
