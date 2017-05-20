fdc69225cb08f5f2a74dd596688764d818f676e5
Joshua <joshuacfang@gmail.com>
2017-05-20 19:36:25 -0400

    clean up promises in test

--- notify/clearrecent.js
+++ notify/clearrecent.js
@@ -1,11 +1,12 @@
 const Event = require('../model/Event');
 const moment = require('moment');
+
 const start = moment().subtract(2, 'd');
 const end = moment().add(1, 'd');
 const model = require('../model/model');
-model.connect();
+
 const eventQuery = Event.find({ date: { $lte: end, $gte: start, }, }).exec();
-eventQuery.then(events => events.forEach((event) => {
+model.connect().then(eventQuery).then(events => events.forEach((event) => {
 	event.status = 'Unscored';
 	event.save();
 }));
